1b137307e9cee376bdf6c4b2786344ae0de58d4a
Itamar Cohen <itamar.c@taboola.com>
2021-07-29 20:43:34 +0300

    - add recent bids fetcher

---
+++ server/db/fetcher.js
@@ -0,0 +1,49 @@
+const _ = require('lodash');
+const { getSortedSet, get } = require('./redis');
+
+const bidsKey = 'LIST_OF_BIDS';
+
+const dateToUnix = (date) => Number((date.getTime() / 1000).toFixed(0));
+let prevTime = dateToUnix(new Date());
+
+const splitIdsAndScores = (bidIdsAndScores) => {
+  const bids = [];
+  for (let i = 0; i < bidIdsAndScores.length; i += 2) {
+    bids.push({ id: bidIdsAndScores[i], time: bidIdsAndScores[i + 1] });
+  }
+  return bids;
+};
+
+const fetchBidData = async (bid) => {
+  const { id, time } = bid;
+  try {
+    const bidData = JSON.parse(await get(id));
+    const bidFullData = { ...bidData, id, time };
+    return bidFullData;
+  } catch (err) {
+    console.err(`error fetching bid id: ${id}`);
+  }
+  return null;
+};
+
+const fetchRecentBids = async (io) => {
+  setTimeout(async () => {
+    const now = dateToUnix(new Date());
+    try {
+      const bidIdsAndScores = await getSortedSet(bidsKey, prevTime - 59, now, 'withscores');
+      const bids = splitIdsAndScores(bidIdsAndScores);
+      const bidPromises = bids.map((bid) => fetchBidData(bid));
+      const bidData = await Promise.all(bidPromises);
+      const campaignToBids = _.groupBy(bidData, 'campaign');
+      Object.keys(campaignToBids).forEach((campaignId) => {
+        io.emit(campaignId, campaignToBids[campaignId]);
+      });
+    } catch (err) {
+      console.log(`error fetching bids: ${err}`);
+    }
+    prevTime = now;
+    fetchRecentBids(io);
+  }, 1000);
+};
+
+module.exports = { fetchRecentBids };
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const { Server } = require('socket.io');
 
+const { fetchRecentBids } = require('./db/fetcher');
 const campaignsRouter = require('./routes/campaigns');
 
 const app = express();
@@ -9,12 +11,24 @@ app.use(cors());
 const server = http.createServer(app);
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use('/campaigns', campaignsRouter);
+
+const io = new Server(server, {
+  cors: {
+    origin: true,
+    credentials: true,
+  },
 });
 
-app.use('/campaigns', campaignsRouter);
+io.on('connection', (socket) => {
+  console.log('new connection: ', socket.id);
+  socket.on('disconnect', () => {
+    console.log('disconnect');
+  });
+});
 
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+fetchRecentBids(io);
